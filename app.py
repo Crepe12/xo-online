79c319b203ecdb2a633bfb57b63633050bc54389
Crepe12 <88484801+Crepe12@users.noreply.github.com>
2025-06-21 22:14:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ if __name__ == '__main__':
     socketio.run(
         app,
         host='0.0.0.0',
-        port=int(os.environ.get('PORT', 10000))
+        port=int(os.environ.get('PORT', 10000)
     )
 
