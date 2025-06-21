1a0e693a200919a7df626786ad6772e634d549d9
Crepe12 <yayaz@gmail.com>
2025-06-21 21:55:15 +0700

    แก้ไขระบบเริ่มเกม XO

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room, emit
+import os
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='threading')  # เปลี่ยนจาก eventlet เป็น threading
+socketio = SocketIO(app)
+
+players = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('make_move')
-def handle_move(data):
-    socketio.emit('move_made', data, broadcast=True)
+@socketio.on('join_room')
+def handle_join(data):
+    room = data['room']
+    join_room(room)
 
-import os
+    if room not in players:
+        players[room] = []
 
-port_env = os.environ.get('PORT')
-port = int(port_env) if port_env and port_env.isdigit() else 10000
+    players[room].append(request.sid)
 
-socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    if len(players[room]) == 2:
+        emit('start_game', {'room': room}, room=room)
+
+if __name__ == '__main__':
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 10000)),
+        allow_unsafe_werkzeug=True
+    )
